30d3f5c0d535dab64220a65a3251122013edddcd
Renzo Fattorini <80908769+renzofatto@users.noreply.github.com>
2022-12-07 23:41:01 -0300

    Update SolucionReto#00.cs

--- app/src/main/java/com/mouredev/weeklychallenge2022/SolucionReto#00.cs
+++ app/src/main/java/com/mouredev/weeklychallenge2022/SolucionReto#00.cs
@@ -1,53 +1,48 @@
-static void Main(string[] args)
- {
-     Console.WriteLine($"Result: {IsAnagram("ael", "tale")}");
- }
+/*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+using System;
+using System.IO;
+using System.Text;
 
- private static bool IsAnagram(string wordOne, string wordTwo)
- {
-     wordOne = wordOne.ToLower();
-     wordTwo = wordTwo.ToLower();
-
-     if (IsSameWordOrDifferentLength(wordOne, wordTwo))
-     {
-         return false;
-     }
-
-     bool isAnagram = true;
-     List<char> wordOneLetters = new();
-     wordOneLetters.AddRange(wordOne);
-
-     foreach (char wordTwoletter in wordTwo)
-     {
-         if (LetterExistRemove(wordOneLetters, wordTwoletter))
-         {
-             isAnagram = false;
-             break;
-         }
-     }
-     return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
-
-     return isAnagram;
- }
-
- private static bool LetterExistRemove(List<char> wordOneLetters, char wordTwoletter)
- {
-     return !wordOneLetters.Remove(wordTwoletter);
- }
-
- private static bool IsSameWordOrDifferentLength(string wordOne, string wordTwo)
- {
-     return IsSameWord(wordOne, wordTwo) ||
-                     !IsSameLength(wordOne, wordTwo);
- }
-
- private static bool IsSameLength(string wordOne, string wordTwo)
- {
-     return wordOne.Length == wordTwo.Length;
- }
-
- private static bool IsSameWord(string wordOne, string wordTwo)
- {
-     return wordOne == wordTwo;
- }
- 
+public class Prueba
+{
+    public static void Main()
+    {
+        FizzBuzz();
+    }
+    private static void FizzBuzz()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i%3==0 && i%5==0)
+            {
+                Console.WriteLine("fizzbuzz");
+            }else if (i%3==0)
+            {
+                Console.WriteLine("buzz");
+            }else if(i%5==0)
+            {
+                Console.WriteLine("fizz");
+            }else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
