26226092665fecb794dcd272a6a2d1659cf51f17
Renzo Fattorini <80908769+renzofatto@users.noreply.github.com>
2022-12-10 13:28:23 -0300

    Solucion en C# del challenge 01

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge1.kt
@@ -30,3 +30,39 @@ private fun isAnagram(wordOne: String, wordTwo: String): Boolean {
     }
     return wordOne.lowercase().toCharArray().sortedArray().contentEquals(wordTwo.lowercase().toCharArray().sortedArray())
 }
+
+
+//---------------------------------------------------
+//C#
+//---------------------------------------------------
+
+
+using System;
+
+public class Challenge01
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Anagrama("jamon","Nojam"));
+    }
+    
+    private static bool Anagrama(string palabra1, string palabra2){
+        string pal1 = palabra1.ToLower();
+        string pal2 = palabra2.ToLower();
+        bool continuar = false;
+        
+        if(!pal1.Equals(pal2) && pal1.Length==pal2.Length){
+            for (int i =0; i< pal1.Length; i++){
+                for(int j=0; j<pal2.Length && !continuar; i++){
+                    if (pal1[i].Equals(pal2[j])){
+                        continuar = true;
+                    }
+                }
+                if (!continuar){
+                    return false;
+                }
+            }
+        }
+        return continuar;
+    }   
+}
